36ac4b75ed0be74c57392736d8dcb7bab472228e
woong <ung91@yahoo.com>
2021-02-12 00:00:13 +0900

    leoshop 초기셋팅

---
+++ leoshop/src/main/java/leobook/leoshop/LeoshopApplication.java
@@ -0,0 +1,14 @@
+package leobook.leoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoshopApplication.class, args);
+	}
+
+}
+ 
---
+++ leoshop/src/test/java/leobook/leoshop/LeoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package leobook.leoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeoshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
