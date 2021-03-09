bed8ad0a865f9d01b3b5b83c72412866624f68b9
woong <ung91@yahoo.com>
2021-03-10 01:06:20 +0900

    MemberService 생성

---
+++ leoshop/src/main/java/leobook/leoshop/domain/service/MemberService.java
@@ -0,0 +1,54 @@
+package leobook.leoshop.domain.service;
+
+import leobook.leoshop.domain.Member;
+import leobook.leoshop.domain.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+       validateDuplicateMember(member);    // 중복 회원 검증증
+       memberRepository.save(member);
+       return member.getId();
+    }
+
+    /**
+     * 중복회원조회
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 조회(단건)
+     * @param memberId
+     * @return
+     */
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
